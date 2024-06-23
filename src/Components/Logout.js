6eed18b075353d2ecd79daa6592cc28507c056b9
cryobus <jaav2801@gmail.com>
2024-06-23 13:05:25 -0400

    logout?

--- src/Components/Logout.js
+++ src/Components/Logout.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
-import axios from 'axios';
 
 const Logout = () => {
   const { logout } = useAuth();
@@ -10,20 +9,20 @@ const Logout = () => {
   useEffect(() => {
     const performLogout = async () => {
       try {
-        const token = localStorage.getItem('token');
-        await axios.post(
-          'https://proyecto2-production-ba5b.up.railway.app/api/logout',
-          {},
-          { headers: { Authorization: `Bearer ${token}` } }
-        );
-        logout();
+        // Asegurarse de cerrar sesión antes de redirigir
+        await logout();
+        // Redirige al usuario a la página de login después de cerrar sesión
         navigate('/login');
       } catch (error) {
         console.error('Error during logout:', error);
         if (error.response && error.response.status === 401) {
-          // Manejar el token expirado
-          logout();
-          navigate('/login');
+          // Si el token ya expiró o es inválido, intenta cerrar sesión de todos modos
+          try {
+            await logout();
+          } finally {
+            // Redirige al usuario a la página de login
+            navigate('/login');
+          }
         }
       }
     };
